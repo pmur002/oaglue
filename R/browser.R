0dd69d99a0b2249d361b72a55e843bb2ca4e38ad
Paul Murrell <paul@stat.auckland.ac.nz>
2014-07-03 12:56:58 +1200

    ripped HTML code out of browser.R - does not belong in basic glue system

--- R/browser.R
+++ R/browser.R
@@ -12,7 +12,7 @@ filterList <- function(x, element) {
 
 makeList <- function(path, element) {
     p <- suppressWarnings(normalizePath(path))
-    files <- list.files(p, pattern="[.]xml$", full=TRUE)
+    files <- list.files(p, pattern="[.]xml$", full.names=TRUE)
     xmlFiles <- lapply(files, filterList, element)
     keep <- !sapply(xmlFiles, is.null)
     list(files=files[keep], xml=xmlFiles[keep])
@@ -53,45 +53,5 @@ pipelineTable <- function(paths) {
     do.call("rbind", pipelineData)
 }
 
-pipelineHTML <- function(file, xml, outdir, outfile) {
-    pipelineName <- basename(file_path_sans_ext(file))
-    p <- readXMLPipeline(xml, pipelineName)
-    graphName <- paste0(pipelineName, ".svg")
-    graphFile <- file.path(outdir, graphName)
-    rag <- agopenTrue(p$graph, "", attrs=list(node=list(shape="ellipse")))
-    pdf(NULL, width=graphWidth(rag), height=graphHeight(rag))
-    grid.graph(rag)
-    grid.export(graphFile)
-    dev.off()
-    cat(paste('<tr><td style="border-style: solid; border-width: 1px">',
-              pipelineName,
-              '</td><td style="border-style: solid; border-width: 1px">',
-              p$desc,
-              '</td><td style="border-style: solid; border-width: 1px">',
-              "<img src='", graphName, "'/>",
-              "</tr>"),
-        file=file.path(outdir, outfile), append=TRUE)
-}
 
-pipelineToHTML <- function(x, dir, file) {
-    # Zero the file
-    writeLines("", file.path(dir, file))
-    cat('<table>', file=file.path(dir, file), append=TRUE)
-    mapply(pipelineHTML, x$files, x$xml, MoreArgs=list(dir, file))
-    cat("</table>", file=file.path(dir, file), append=TRUE)
-}
-
-pipelineHTMLTable <- function(paths, file="pipelines.html", dir="HTML") {
-    require(gridGraphviz)
-    require(gridSVG)
-    if (!file.exists(dir))
-        dir.create(dir)
-    pathv <- splitPath(paths)
-    fileList <- lapply(pathv, makeList, "pipeline")
-    lapply(fileList, pipelineToHTML, dir, file)
-}
-
-# Example usage ...
-#  library(googleVis)
-#  plot(gvisTable(as.data.frame(moduleTable("XML"))))
 
